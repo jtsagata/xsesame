51b69a90e4022abe0c7316e66b66d267fdc201d2
Giannis Tsagkatakis <jtsagata@gmail.com>
2021-02-13 15:59:46 +0200

    Add more checks to the parser

--- src/desktop_info.rs
+++ src/desktop_info.rs
@@ -10,9 +10,27 @@ pub struct DesktopInfo {
 }
 
 impl DesktopInfo {
-  pub fn new(path: String, desktop: Entry) -> Self {
-    // TODO: check if provides the filed we need and maybe return an Optional
-    DesktopInfo { path, desktop }
+  pub fn new(path: String, desktop: Entry) -> Result<Self,String> {
+    let path_copy = path.clone();
+    let entry = DesktopInfo{ path, desktop };
+
+    // Test Type="Application"
+    let d_type = entry.get_attribute("Type");
+    match d_type {
+      None => {
+        return Err(format!("The file {} does not specify a type", path_copy));
+      }
+      Some(d_type) => if d_type != "Application" {
+        return Err(format!("The file {} specify type as '{}'", path_copy, d_type));
+      },
+    }
+
+    // Test if provides a Name
+    if entry.get_attribute("Name").is_none() {
+      return Err(format!("The file {} does not specify a name", path_copy));
+    }
+
+    Ok(entry)
   }
 
   pub fn path(&self) -> &str {
@@ -20,23 +38,25 @@ impl DesktopInfo {
   }
 
   pub fn name(&self) -> &str {
-    self.get_attribute("Name")
+    self.get_attribute_str("Name")
   }
 
   pub fn icon(&self) -> &str {
-    self.get_attribute("Icon")
+    self.get_attribute_str("Icon")
   }
 
   pub fn comment(&self) -> &str {
     // TODO: Localized
-    self.get_attribute("Comment")
+    self.get_attribute_str("Comment")
   }
 
-  fn get_attribute(&self, attr: &str) -> &str {
-    let comment = self.desktop.section("Desktop Entry").attr(attr);
-    Option::or(comment, Some("")).unwrap()
+  pub fn get_attribute(&self, attr: &str) -> Option<&str>{
+    return self.desktop.section("Desktop Entry").attr(attr);
   }
 
+  pub fn get_attribute_str(&self, attr: &str) -> &str {
+    Option::or(self.get_attribute(&attr), Some("")).unwrap()
+  }
 
   pub fn active_str(&self) -> &str {
     let path = Path::new(self.path());
@@ -71,8 +91,15 @@ impl DesktopInfo {
       match desktop_entry {
         Ok(desktop_entry) => {
           let desktop = DesktopInfo::new(file_path, desktop_entry);
-          println!("{}: [{}] '{}' {} -- {}", map_key, desktop.icon(), desktop.name(), desktop.active_str(), desktop.comment());
-          sessions.entry(map_key).or_insert(desktop);
+          match desktop {
+            Ok(desktop) => {
+              println!("{}: [{}] '{}' {} -- {}", map_key, desktop.icon(), desktop.name(), desktop.active_str(), desktop.comment());
+              sessions.entry(map_key).or_insert(desktop);
+            }
+            Err(error) => {
+              println!("*** ERROR {}", error)
+            }
+          }
         }
         Err(_) => {
           // TODO: To error stream
