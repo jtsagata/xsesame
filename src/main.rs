a761b96fc7a93feb2cd7e0972a0f3e37266aae98
Giannis Tsagkatakis <jtsagata@gmail.com>
2021-02-14 00:12:52 +0200

    Command line options, table print with style a shell completions

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,55 @@
 mod desktop_info;
+mod opts;
 
-use std::io;
-use crate::desktop_info::{DesktopInfo};
-use std::collections::HashMap;
+// use crate::desktop_info::DesktopInfo;
 use stybulate::{Table, Style, Headers, Cell};
+use desktop_info::DesktopInfo;
+use std::collections::HashMap;
 use std::path::Path;
 use std::ffi::OsStr;
+use clap::{Shell};
 use colored::*;
-
-// TODO: From cargo? or from command line arguments :-) -- solved
-// const XSESSION_DIR: &str = "/usr/share/xsessions/";
-const XSESSION_DIR: &str = "/home/talos/CLionProjects/Rust/sesman/test/samples";
+use std::io;
 
 #[cfg(target_os = "linux")]
-fn main() -> io::Result<()> {
-  cmd_list_sessions();
-  Ok(())
+fn main() {
+  let matches = opts::build_cli().get_matches();
+
+  // Sessions directory
+  let xsession_dir = matches.value_of("session-dir").unwrap_or(opts::XSESSION_DIR);
+  if !Path::new(xsession_dir).is_dir() {
+    //TODO: Add better error handling and error codes
+    panic!(format!("{}: is not a directory", xsession_dir));
+  }
+
+  if let Some(matches) = matches.subcommand_matches("list") {
+    let style = matches.value_of("style").unwrap_or("Fancy");
+    let style = match style {
+      "Fancy" => { Style::Fancy }
+      "Grid" => { Style::Grid }
+      "Simple" => { Style::Simple }
+      &_ => { Style::Plain }
+    };
+    cmd_list_sessions(xsession_dir, style);
+  }
+
+  if let Some(matches) = matches.subcommand_matches("completion") {
+    let shell = matches.value_of("shell").unwrap_or("bash");
+    let shell = match shell
+    {
+      "zsh" => { Shell::Zsh }
+      "fish" => { Shell::Fish }
+      "elvish" => { Shell::Elvish }
+      &_ => { Shell::Bash }
+    };
+    opts::build_cli().gen_completions_to("xsesame", shell, &mut io::stdout());
+  }
 }
 
-fn cmd_list_sessions() {
+fn cmd_list_sessions(xsession_dir: &str, style: stybulate::Style) {
   let mut sessions = HashMap::<String, DesktopInfo>::new();
 
-  if DesktopInfo::collect_sessions(&mut sessions, XSESSION_DIR).is_err() {
+  if DesktopInfo::collect_sessions(&mut sessions, xsession_dir).is_err() {
     panic!("Unable to parse sessions");
   }
 
@@ -34,18 +62,19 @@ fn cmd_list_sessions() {
   }
 
   let table = Table::new(
-    Style::Fancy,
-    elements,
+    style, elements,
     Some(Headers::from(vec!["Key", "Name", "Comment"])),
   ).tabulate();
 
   println!("List of active and inactive sessions:");
+  println!();
   println!("{}", table);
-  println!("To enable/disable a session run: {} {}", program_name().unwrap().green(), "enable|disable <key>".green() );
+  println!();
+  println!("To enable/disable a session run: {} {}", program_name().unwrap().green(), "enable|disable <key>".green());
   println!();
 }
 
-fn program_name() -> Option<String>  {
+fn program_name() -> Option<String> {
   std::env::current_exe().ok()
     .as_ref()
     .map(Path::new)
---
+++ src/opts.rs
@@ -0,0 +1,71 @@
+
+use clap::{SubCommand, Arg, crate_authors, crate_version, AppSettings, App};
+
+pub const XSESSION_DIR: &str = "/usr/share/xsessions";
+
+
+pub fn build_cli() -> App<'static, 'static> {
+  let help_start = "\nSome desktop environments install a lot of different types that have to real use for
+the end user. For example cinnamon also install a cinnamon fallback. Others install a lot more.
+This small utility helps you to list and disable some of them. Of course you can also re-enable them.
+
+The propose of this little tool is to minimize the clutter in the display manager.";
+
+  App::new("xsesame")
+    .version(crate_version!())
+    .author(crate_authors!("\n"))
+    .about("Manage display manager sessions. Allow to list, enable and disable them")
+    .after_help("Use help <subcommand> for more info.")
+    .long_about(help_start)
+
+    .setting(AppSettings::UnifiedHelpMessage)
+    .setting(AppSettings::ColoredHelp)
+    .setting(AppSettings::ColorAuto)
+    .setting(AppSettings::GlobalVersion)
+    .setting(AppSettings::InferSubcommands)
+
+    .arg(Arg::with_name("session-dir")
+      .long("session-dir").short("d")
+      .value_name("XSESSION_DIR").takes_value(true)
+      .default_value(XSESSION_DIR)
+      .require_equals(true)
+      .hide_default_value(true)
+      .global(true)
+      .help("Session config directory")
+    )
+
+    .subcommand(
+      SubCommand::with_name("export")
+        .about("Export session list")
+        .setting(AppSettings::Hidden)
+    )
+
+    .subcommand(
+      SubCommand::with_name("completion")
+        .about("Generate completions for various shells")
+        .arg(Arg::with_name("shell")
+          .short("s").long("shell")
+          .require_equals(true)
+          .possible_values(&["bash","zsh","fish","elvish"])
+          .default_value("bash")
+          .takes_value(true).value_name("shell")
+          .hide_default_value(true)
+          .help("shell to generate completions")
+        )
+    )
+
+    .subcommand(
+      SubCommand::with_name("list")
+        .about("list display manager sessions")
+        .long_about(help_start)
+        .display_order(1)
+        .arg(Arg::with_name("style")
+          .short("s").long("style")
+          .possible_values(&["Plain", "Fancy", "Grid", "Simple"])
+          .takes_value(true).value_name("Style")
+          .default_value("Fancy")
+          .hide_default_value(true)
+          .help("Display table with style")
+        )
+    )
+}
